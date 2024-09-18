b94ea44f81013b55cdbce8bc5d42082195d26818
Abhithakur04 <152187846+Abhithakur04@users.noreply.github.com>
2024-09-18 10:34:07 +0530

    Add files via upload

---
+++ PGllife/includes/head_links.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="css/bootstrap.min.css ">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/all.min.css">
+<link rel="stylesheet" href="css/fontawesome.min.css">
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/common.css">
+<link rel="stylesheet" href="css/property_list.css">
+<link rel="stylesheet" href="css/property_detail.css">
+<link rel="stylesheet" href="css/dashboard.css">
